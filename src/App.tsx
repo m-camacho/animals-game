d08b2f7a067d47ebce304e3e7189b148ef9f2fcc
Mario Camacho <mario.camacho@lime-energy.com>
2022-01-06 19:28:03 -0400

    Adding bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,38 @@
 import React, { useEffect, useState } from "react";
 import "./App.scss";
 import { Animal, animals } from "./constants";
+import Button from "react-bootstrap/Button";
 
 function App() {
-  const [randomAnimal, setRandomAnimal] = useState<Animal | null>(null);
+  const [hiddenAnimal, setHiddenAnimal] = useState<Animal | null>(null);
 
   useEffect(() => {
     const randomIndex = Math.floor(Math.random() * (animals.length + 1));
-    setRandomAnimal(animals[randomIndex]);
+    setHiddenAnimal(animals[randomIndex]);
   }, []);
 
   return (
     <div className="game">
-      {randomAnimal && (
-        <div>
-          <div>{randomAnimal.name}</div>
-          <img src={randomAnimal.imageUrl} alt={randomAnimal.name} />
+      <div className="option-buttons">
+        {animals.map(animal => (
+          <Button
+            key={animal.name}
+            onClick={() => {
+              if (hiddenAnimal && animal.name === hiddenAnimal.name) {
+                console.log("GANASTE!!!!");
+              } else {
+                console.log("ERROR");
+              }
+            }}
+          >
+            {animal.name}
+          </Button>
+        ))}
+      </div>
+      {hiddenAnimal && (
+        <div className="hidden-animal">
+          <div className="name">{hiddenAnimal.name}</div>
+          <img src={hiddenAnimal.imageUrl} alt={hiddenAnimal.name} />
         </div>
       )}
     </div>
