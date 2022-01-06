7db474d42ef0e7b46b57fc7ea65195d2078ac50b
Mario Camacho <mario.camacho@lime-energy.com>
2022-01-06 18:45:21 -0400

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import { Animal, animals } from "./constants";
 
 function App() {
+  const [randomAnimal, setRandomAnimal] = useState<Animal | null>(null);
+
+  useEffect(() => {
+    const randomIndex = Math.floor(Math.random() * (animals.length + 1));
+    setRandomAnimal(animals[randomIndex]);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="game">
+      {randomAnimal && (
+        <div>
+          <div>{randomAnimal.name}</div>
+          <img src={randomAnimal.imageUrl} alt={randomAnimal.name} />
+        </div>
+      )}
     </div>
   );
 }
---
+++ src/constants.tsx
@@ -0,0 +1,51 @@
+export interface Animal {
+  name?: string;
+  imageUrl?: string;
+}
+
+export const animals: Animal[] = [
+  {
+    name: "gato",
+    imageUrl:
+      "https://ca-times.brightspotcdn.com/dims4/default/870ca28/2147483647/strip/true/crop/2048x1108+0+0/resize/840x454!/quality/90/?url=https%3A%2F%2Fcalifornia-times-brightspot.s3.amazonaws.com%2F12%2Fa5%2F79e097ccf62312d18a025f22ce48%2Fhoyla-recuento-11-cosas-aman-gatos-top-001"
+  },
+  {
+    name: "perro",
+    imageUrl:
+      "https://phantom-expansion.unidadeditorial.es/04d5a7930d32e4860ba7cf82291b9d30/resize/640/assets/multimedia/imagenes/2021/03/18/16160714974849.jpg"
+  },
+  {
+    name: "caballo",
+    imageUrl:
+      "https://concepto.de/wp-content/uploads/2021/07/caballos-e1626738224231.jpg"
+  },
+  {
+    name: "loro",
+    imageUrl:
+      "https://estaticos-cdn.elperiodico.com/clip/9a36bb77-0c88-4b3c-a7dc-f3d41dd85987_alta-libre-aspect-ratio_default_0.jpg"
+  },
+  {
+    name: "cocodrilo",
+    imageUrl:
+      "https://cdni.rt.com/actualidad/public_images/2021.09/article/61400c73e9ff715a552891d1.jpg"
+  },
+  {
+    name: "elefante",
+    imageUrl: "https://static.dw.com/image/45665028_303.jpg"
+  },
+  {
+    name: "tucan",
+    imageUrl:
+      "https://www.mascotadomestica.com/ezoimgfmt/i1.wp.com/www.mascotadomestica.com/wp-content/uploads/2016/11/rsz_maxresdefault-1.jpg?ezimgfmt=ng%3Awebp%2Fngcb1%2Frs%3Adevice%2Frscb1-2&resize=580%2C381&ssl=1"
+  },
+  {
+    name: "tiburon",
+    imageUrl:
+      "https://img.vixdata.io/pd/jpg-large/es/sites/default/files/n/nukumi_la_hembra_tiburon_reina_del_oceano.jpg"
+  },
+  {
+    name: "pinguino",
+    imageUrl:
+      "https://images.ecestaticos.com/pqIAcGCEagnkjdIBVKVbC9i5FH4=/0x0:1920x1278/1200x900/filters:fill(white):format(jpg)/f.elconfidencial.com%2Foriginal%2Fe8e%2Fe27%2F2bf%2Fe8ee272bfd36f69679936351209d708c.jpg"
+  }
+];
